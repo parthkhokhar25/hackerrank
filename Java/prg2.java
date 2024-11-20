ea177509273e30a221ed6d1bc8c9d8b37874d705
parthkhokhar25 <parthkhokhar61@gmail.com>
2024-11-20 23:39:06 +0530

    programming with java

---
+++ Java/prg2.java
@@ -0,0 +1,46 @@
+/*
+Task
+
+    In this challenge, you must read  integers from stdin and then print them to stdout. 
+    Each integer must be printed on a new line. 
+    To make the problem a little easier, a portion of the code is provided for you in the editor below.
+-----------------------------------------------------------------------------------------------------------------
+Input Format
+
+There are  lines of input, and each line contains a single integer.
+
+Sample Input
+
+42
+100
+125
+
+Sample Output
+
+42
+100
+125
+
+*/
+
+
+import java.util.*;
+
+class prg2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        scanner.close();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
